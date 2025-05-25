5e63e46de4b42970803494f6f540e6d8dd9e8897
saurabh2097 <saurabhchimankar97@gmail.com>
2025-05-25 11:28:21 +0530

    Update ubuntu-jenkins.sh

--- ubuntu-jenkins.sh
+++ ubuntu-jenkins.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt install openjdk-17-jre-headless -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
